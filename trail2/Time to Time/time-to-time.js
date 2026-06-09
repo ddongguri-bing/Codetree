76bfa15ba23c43073c7896c2034cde31a6c0e759
ddongguri-bing <137764022+ddongguri-bing@users.noreply.github.com>
2026-06-09 09:41:01 +0900

    [Passed] Time to Time (67ms, 10MB)

---
+++ trail2/Time to Time/time-to-time.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [a, b, c, d] = input[0].split(' ').map(Number);
+// Please Write your code here.
+
+const start = a * 60 + b;
+const end = c * 60 + d;
+
+
+
+console.log(end - start);
